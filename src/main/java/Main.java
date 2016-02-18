394edaf0c8b068d529ba26752949b50df9310343
Yishi1215 <yishi1215@gmail.com>
2016-02-18 18:07:40 -0500

    revise java code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -31,12 +31,12 @@ public class Main {
       return "E=mc^2: 12 GeV = " + m.toString();
     });
 
-    get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
+    // get("/", (request, response) -> {
+    //         Map<String, Object> attributes = new HashMap<>();
+    //         attributes.put("message", "Hello World!");
 
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
+    //         return new ModelAndView(attributes, "index.ftl");
+    //     }, new FreeMarkerEngine());
 
     get("/db", (req, res) -> {
       Connection connection = null;
